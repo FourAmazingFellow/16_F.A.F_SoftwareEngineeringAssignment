bf3260db280454a0ee965ddf1c0bf7ab553b771d
Accident <842631684@qq.com>
2016-10-16 14:51:51 +0800

    添加VO

--- Codes/Hotel_Client/src/vo/BriefOrderInfoVO.java
+++ Codes/Hotel_Client/src/vo/BriefOrderInfoVO.java
@@ -1,5 +1,108 @@
 package vo;
 
-public class BriefOrderInfoVO {
+import java.util.Date;
 
+import po.RoomType;
+
+/**
+ * 简要订单信息的值对象
+ * @author Accident
+ * @version 
+ * @see
+ */
+public class BriefOrderInfoVO {
+	private long userID;
+	private String orderID;
+	private String hotelName;
+	private String hotelAddress;
+	private Date beginDate;
+	private Date finishDate;
+	private RoomType roomType;
+	private int num;
+	private int totalPrice;
+
+	public BriefOrderInfoVO(long userID, String orderID, String hN, String hA, Date bD,
+			Date fD, RoomType rT, int n, int tP) {
+		this.orderID = orderID;
+		this.userID = userID;
+		hotelName = hN;
+		hotelAddress = hA;
+		beginDate = bD;
+		finishDate = fD;
+		roomType = rT;
+		num = n;
+		totalPrice = tP;
+	}
+
+	public long getUserID() {
+		return userID;
+	}
+
+	public String getOrderID(){
+		return orderID;
+	}
+	
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public String getHotelAddress() {
+		return hotelAddress;
+	}
+
+	public Date getBeginDate() {
+		return beginDate;
+	}
+
+	public Date getFinishDate() {
+		return finishDate;
+	}
+
+	public RoomType getRoomType() {
+		return roomType;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setUserID(long userID) {
+		this.userID = userID;
+	}
+
+	public void setOrderID(String orderID){
+		this.orderID = orderID;
+	}
+	
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public void setHotelAddress(String hotelAddress) {
+		this.hotelAddress = hotelAddress;
+	}
+
+	public void setBeginDate(Date beginDate) {
+		this.beginDate = beginDate;
+	}
+
+	public void setFinishDate(Date finishDate) {
+		this.finishDate = finishDate;
+	}
+
+	public void setRoomType(RoomType roomType) {
+		this.roomType = roomType;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
 }
--- Codes/Hotel_Client/src/vo/OrderVO.java
+++ Codes/Hotel_Client/src/vo/OrderVO.java
@@ -1,11 +1,95 @@
 package vo;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import po.OrderState;
+import po.RoomType;
+
 /**
- * 
- * @author 原
- * @version
+ * 详细订单信息的值对象
+ * @author Accident
+ * @version 1.0
  * @see
  */
-public class OrderVO {
+public class OrderVO extends BriefOrderInfoVO{	
+	private Calendar orderProducedTime;
+	private Calendar lastedOrderDoneTime;
+	private int numOfPerson;
+	private boolean isChildren;
+	private boolean isOnSale;
+	private OrderState orderState;
+	private boolean isCommented;
 
+	public OrderVO(long userID,String orID, String hN, String hA, Date bD, Date fD, RoomType rT, int n, int tP,
+			String ID, Calendar oPT, Calendar lODT, int nOP, boolean isChild,
+			boolean isOnSale, OrderState orderS, boolean isCom) {
+		
+		super(userID, orID, hN, hA, bD, fD, rT, n, tP);
+		
+		orderProducedTime = oPT;
+		lastedOrderDoneTime = lODT;
+		numOfPerson = nOP;
+		isChildren = isChild;
+		this.isOnSale = isOnSale;
+		orderState = orderS;
+		isCommented = isCom;
+	}
+
+	public Calendar getOrderProducedTime() {
+		return orderProducedTime;
+	}
+
+	public Calendar getLastedOrderDoneTime() {
+		return lastedOrderDoneTime;
+	}
+
+	public int getNumOfPerson() {
+		return numOfPerson;
+	}
+
+	public boolean isChildren() {
+		return isChildren;
+	}
+
+	public boolean isOnSale() {
+		return isOnSale;
+	}
+
+	public OrderState getOrderState() {
+		return orderState;
+	}
+
+	public boolean isCommented() {
+		return isCommented;
+	}
+
+	public void setOrderProducedTime(Calendar orderProducedTime) {
+		this.orderProducedTime = orderProducedTime;
+	}
+
+	public void setLastedOrderDoneTime(Calendar lastedOrderDoneTime) {
+		this.lastedOrderDoneTime = lastedOrderDoneTime;
+	}
+
+	public void setNumOfPerson(int numOfPerson) {
+		this.numOfPerson = numOfPerson;
+	}
+
+	public void setChildren(boolean isChildren) {
+		this.isChildren = isChildren;
+	}
+
+	public void setOnSale(boolean isOnSale) {
+		this.isOnSale = isOnSale;
+	}
+
+	public void setOrderState(OrderState orderState) {
+		this.orderState = orderState;
+	}
+
+	public void setCommented(boolean isCommented) {
+		this.isCommented = isCommented;
+	}
+	
 }
