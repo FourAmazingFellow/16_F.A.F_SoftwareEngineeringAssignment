3a6991f4246430ba8c43f9ca82b9e670c5017bc4
Accident <842631684@qq.com>
2016-10-16 16:35:14 +0800

    啊哈

--- Codes/Hotel_Client/src/bl_Stub/orderblservice_Stub/GetOrderDoneServiceImpl_Stub.java
+++ Codes/Hotel_Client/src/bl_Stub/orderblservice_Stub/GetOrderDoneServiceImpl_Stub.java
@@ -13,30 +13,35 @@ public class GetOrderDoneServiceImpl_Stub implements GetOrderDoneService {
 	@Override
 	public ArrayList<BriefOrderInfoVO> getHotelOrderList(String address, Enum<OrderType> orderType) {
 		// TODO Auto-generated method stub
+		System.out.println("得到酒店所有订单列表");
 		return null;
 	}
 
 	@Override
 	public OrderVO getSingleOrder(String address, String orderID) {
 		// TODO Auto-generated method stub
+		System.out.println("得到F.A.F自主豪华八星级酒店的对应订单");
 		return null;
 	}
 
 	@Override
 	public boolean setOrderState(Enum<OrderState> orderstate, OrderVO vo) {
 		// TODO Auto-generated method stub
+		System.out.println("更改订单状态");
 		return false;
 	}
 
 	@Override
 	public boolean getOrderDone(OrderVO vo) {
 		// TODO Auto-generated method stub
+		System.out.println("执行订单");
 		return false;
 	}
 
 	@Override
 	public boolean delayCheckIn(OrderVO vo, int hours) {
 		// TODO Auto-generated method stub
+		System.out.println("延迟入住");
 		return false;
 	}
 
