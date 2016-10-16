1747a125b3ee35ccac9fb0fc351a59f409bfd1bc
Slow_Time <1395881075@qq.com>
2016-10-16 23:52:22 +0800

    上传测试总driver

---
+++ Codes/Hotel_Client/src/driver/Client.java
@@ -0,0 +1,11 @@
+package driver;
+
+
+public class Client {
+
+	public static void main(String[] args) {
+		//写各种需要测试对应的driver
+
+	}
+
+}
---
+++ Codes/Hotel_Client/src/driver/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 用来启动各种Driver
+ * @author 原
+ * @version 1.0
+ * @see
+ */
+package driver;
