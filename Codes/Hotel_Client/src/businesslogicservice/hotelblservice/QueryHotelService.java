7378f817c1c87946ad15dd9df12acd11eae45b14
subZHS <151250008@smail.nju.edu.cn>
2016-10-23 16:42:48 +0800

    。。。

--- Codes/Hotel_Client/src/businesslogicservice/hotelblservice/QueryHotelService.java
+++ Codes/Hotel_Client/src/businesslogicservice/hotelblservice/QueryHotelService.java
@@ -13,30 +13,30 @@ import vo.OrderVO;
  * @see
  */
 public interface QueryHotelService {
-	
-	/**
-	 * 查找符合输入条件的酒店
-	 * @param condition String[]型，界面传递来的查看条件
-	 * @return 返回符合输入条件的所有酒店的简要信息列表
-	 * @see
-	 */
-	public ArrayList<BriefHotelInfoVO> getHotelBriefInfoListByQuerying (String[] condition);
-	
-	/**
-	 * 获取酒店详细信息
-	 * @param address String型，界面传递来的酒店地址
-	 * @return 返回酒店详细信息
-	 * @see
-	 */
-	public HotelVO getHotelDetails(String address);
-	
-	/**
-	 * 获取用户在该酒店的所有订单
-	 * @param address String型，界面传递来的酒店地址
-	 * @param ID long型，界面传递来的用户标识
-	 * @return 返回用户在该酒店的所有订单的列表
-	 * @see
-	 */
-	public ArrayList<OrderVO> getOrders(String address, String userID);
+    
+    /**
+     * 查找符合输入条件的酒店
+     * @param condition String[]型，界面传递来的查看条件
+     * @return 返回符合输入条件的所有酒店的简要信息列表
+     * @see
+     */
+    public ArrayList<BriefHotelInfoVO> getHotelBriefInfoListByQuerying (String[] condition);
+    
+    /**
+     * 获取酒店详细信息
+     * @param address String型，界面传递来的酒店地址
+     * @return 返回酒店详细信息
+     * @see
+     */
+    public HotelVO getHotelDetails(String address);
+    
+    /**
+     * 获取用户在该酒店的所有订单
+     * @param address String型，界面传递来的酒店地址
+     * @param ID long型，界面传递来的用户标识
+     * @return 返回用户在该酒店的所有订单的列表
+     * @see
+     */
+    public ArrayList<OrderVO> getOrders(String address, String userID);
 
 }
--- Codes/Hotel_Client/src/businesslogicservice/orderblservice/WithdrawOrderService.java
+++ Codes/Hotel_Client/src/businesslogicservice/orderblservice/WithdrawOrderService.java
@@ -13,45 +13,45 @@ import vo.OrderVO;
  * @see
  */
 public interface WithdrawOrderService {
-	
-	/**
-	 * 得到用户所有订单
-	 * @param userID String型 客户ID
-	 * @param orderType 订单类型
-	 * @return 按orderType的值进行查找并按生成时间顺序排列的订单列表
-	 * @see
-	 */
-	public ArrayList<BriefOrderInfoVO> getUserOrderList (String userID, Enum<OrderType> orderType);
-	
-	/**
-	 * 得到订单详细信息
-	 * @param orderID 订单号
-	 * @return 该订单号对应的OrderVO
-	 * @see
-	 */
-	public OrderVO getDetailedOrder(String orderID);
-	
-	/**
-	 * 判断该订单是否可以被撤销
-	 * @param vo 订单VO
-	 * @return 该订单当前是否可以被撤销
-	 * @see
-	 */
-	public boolean isWithdrawable(OrderVO vo); 
+    
+    /**
+     * 得到用户所有订单
+     * @param userID String型 客户ID
+     * @param orderType 订单类型
+     * @return 按orderType的值进行查找并按生成时间顺序排列的订单列表
+     * @see
+     */
+    public ArrayList<BriefOrderInfoVO> getUserOrderList (String userID, Enum<OrderType> orderType);
+    
+    /**
+     * 得到订单详细信息
+     * @param orderID 订单号
+     * @return 该订单号对应的OrderVO
+     * @see
+     */
+    public OrderVO getDetailedOrder(String orderID);
+    
+    /**
+     * 判断该订单是否可以被撤销
+     * @param vo 订单VO
+     * @return 该订单当前是否可以被撤销
+     * @see
+     */
+    public boolean isWithdrawable(OrderVO vo); 
 
-	/**
-	 * 判断此次撤销操作是否需要扣除信用值
-	 * @param vo 订单VO
-	 * @return 此次撤销操作是否需要扣除信用值
-	 * @see
-	 */
-	public boolean isTooLate(OrderVO vo); 
+    /**
+     * 判断此次撤销操作是否需要扣除信用值
+     * @param vo 订单VO
+     * @return 此次撤销操作是否需要扣除信用值
+     * @see
+     */
+    public boolean isTooLate(OrderVO vo); 
 
-	/**
-	 * 撤销订单（（扣除信用值），系统将订单置为撤销状态，记录撤销时间，更新空房列表）
-	 * @param vo 订单VO
-	 * @return boolean 
-	 * @see
-	 */
-	public boolean withdrawOrder(OrderVO vo); 
+    /**
+     * 撤销订单（（扣除信用值），系统将订单置为撤销状态，记录撤销时间，更新空房列表）
+     * @param vo 订单VO
+     * @return boolean 
+     * @see
+     */
+    public boolean withdrawOrder(OrderVO vo); 
 }
