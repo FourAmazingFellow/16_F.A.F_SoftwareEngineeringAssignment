d92223c306e834312e3038eb6c40224db1bbf928
Accident <842631684@qq.com>
2016-11-20 16:31:48 +0800

    loginPanel

--- Codes/HotelReserveSystem_Client/src/main/java/view/MainApp.java
+++ Codes/HotelReserveSystem_Client/src/main/java/view/MainApp.java
@@ -2,15 +2,19 @@ package view;
 
 import java.io.IOException;
 
+import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import view.client.SearchPanelController;
+import view.login.LoginController;
 
 public class MainApp extends Application {
 	private Stage primaryStage;
+	private AnchorPane loginPanel;
 	private AnchorPane searchPanel;
 
 	@Override
@@ -19,14 +23,32 @@ public class MainApp extends Application {
 		this.primaryStage.setTitle("F.A.F 酒店预定系统");
 		this.primaryStage.setResizable(false);
 
-		showClientView();
+		showLoginView();
 	}
+	
+	public void showLoginView() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("login/Login.fxml"));
+			loginPanel = (AnchorPane) loader.load();
+			
+			Scene scene = new Scene(loginPanel);
+			primaryStage.setScene(scene);
 
+			LoginController controller = loader.getController();
+			controller.setMainApp(this);
+
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void showClientView() {
 
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("client/searchPanel.fxml"));
+			loader.setLocation(MainApp.class.getResource("client/SearchPanel.fxml"));
 			searchPanel = (AnchorPane) loader.load();
 			
 			// Show the scene containing the root layout.
