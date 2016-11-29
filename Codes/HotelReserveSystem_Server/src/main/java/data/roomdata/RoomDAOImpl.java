c0675392ad0abf72a06dd6f4d53f0d638dc1a024
Slow_Time <1395881075@qq.com>
2016-11-29 20:12:02 +0800

    完成roomDAOImpl的所有方法以及测试

--- Codes/HotelReserveSystem_Server/src/main/java/data/roomdata/RoomDAOImpl.java
+++ Codes/HotelReserveSystem_Server/src/main/java/data/roomdata/RoomDAOImpl.java
@@ -1,8 +1,16 @@
 package data.roomdata;
 
 import java.rmi.RemoteException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
+
+import data.databaseutility.JDBC_Connection;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import dataservice.roomDAO.RoomDAO;
 import po.CheckInPO;
@@ -12,76 +20,369 @@ import po.RoomType;
 
 public class RoomDAOImpl implements RoomDAO {
 
-    @Override
+	//将数据库中储存房间类型的int型转换成RoomType型
+	private RoomType convertFromIntToRoomType(int roomType) {
+		switch(roomType) {
+		case 0:
+			return RoomType.SINGLE_ROOM;
+		case 1:
+			return RoomType.STANDARD_ROOM;
+		case 2:
+			return RoomType.TRIBLE_ROOM;
+		default:
+			return RoomType.KING_SIZE_ROOM;
+		}
+	}	
+	
+	//将RoomPO中储存房间类型的RoomType型转换成int型
+	private int convertFromRoomTypeToInt(Enum<RoomType> roomType) {
+		if(roomType == RoomType.SINGLE_ROOM)
+			return 0;
+		else if(roomType == RoomType.STANDARD_ROOM)
+			return 1;
+		else if(roomType == RoomType.TRIBLE_ROOM)
+			return 2;
+		else
+			return 3;
+	}	
+	
+    @SuppressWarnings("deprecation")
+	@Override
     public ArrayList<RoomPO> getSpareRoomInfoList(String address, Date day) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RoomPO> roomPOs = new ArrayList<>();
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from room" + (day.getMonth() + 1) + (day.getDate())+ " where hotelAddress = ?");
+			pstmt.setString(1, address);
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				RoomPO roomPO = new RoomPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), rs.getInt("roomPrice"), address);
+				roomPOs.add(roomPO);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return roomPOs;
     }
 
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     public RoomPO getSpareRoomInfo(String address, Enum<RoomType> roomType, Date day) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		RoomPO roomPO = null;
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from room" + (day.getMonth() + 1) + (day.getDate())+ " where hotelAddress = ? and roomType = ?");
+			pstmt.setString(1, address);
+			pstmt.setInt(2, convertFromRoomTypeToInt(roomType));
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				roomPO = new RoomPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), rs.getInt("roomPrice"), address);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return roomPO;
     }
 
     @Override
     public ArrayList<RoomPO> getCheckInInfoList(String address) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RoomPO> checkInPOs = new ArrayList<>();
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from checkin where hotelAddress = ?");
+			pstmt.setString(1, address);
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				CheckInPO checkInPO = new CheckInPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), address, rs.getTimestamp("checkInTime"), rs.getTimestamp("expDepartTime"));
+				checkInPOs.add(checkInPO);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return checkInPOs;
     }
 
     @Override
     public ArrayList<RoomPO> getCheckInInfo(String address, Enum<RoomType> roomType) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RoomPO> checkInPOs = new ArrayList<>();
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from checkin where hotelAddress = ? and roomType = ?");
+			pstmt.setString(1, address);
+			pstmt.setInt(2, convertFromRoomTypeToInt(roomType));
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				CheckInPO checkInPO = new CheckInPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), address, rs.getTimestamp("checkInTime"), rs.getTimestamp("expDepartTime"));
+				checkInPOs.add(checkInPO);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return checkInPOs;
     }
 
     @Override
     public ArrayList<RoomPO> getCheckInInfo(String address, Date startTime, Date endTime) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RoomPO> checkInPOs = new ArrayList<>();
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from checkin where hotelAddress = ? and checkInTime between ? and ?");
+			pstmt.setString(1, address);
+			pstmt.setTimestamp(2, (new java.sql.Timestamp(startTime.getTime())));
+			pstmt.setTimestamp(3, (new java.sql.Timestamp(endTime.getTime())));
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				CheckInPO checkInPO = new CheckInPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), address, rs.getTimestamp("checkInTime"), rs.getTimestamp("expDepartTime"));
+				checkInPOs.add(checkInPO);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return checkInPOs;
     }
 
     @Override
     public ArrayList<RoomPO> getCheckOutInfoList(String address) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RoomPO> checkOutPOs = new ArrayList<>();
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from checkout where hotelAddress = ?");
+			pstmt.setString(1, address);
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				CheckOutPO checkOutPO = new CheckOutPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), address, rs.getTimestamp("actDepartTime"));
+				checkOutPOs.add(checkOutPO);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return checkOutPOs;
     }
 
     @Override
     public ArrayList<RoomPO> getCheckOutInfo(String address, Date startTime, Date endTime) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RoomPO> checkOutPOs = new ArrayList<>();
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from checkout where hotelAddress = ? and actDepartTime between ? and ?");
+			pstmt.setString(1, address);
+			pstmt.setTimestamp(2, (new java.sql.Timestamp(startTime.getTime())));
+			pstmt.setTimestamp(3, (new java.sql.Timestamp(endTime.getTime())));
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				CheckOutPO checkOutPO = new CheckOutPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), address, rs.getTimestamp("actDepartTime"));
+				checkOutPOs.add(checkOutPO);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return checkOutPOs;
     }
 
     @Override
     public ArrayList<RoomPO> getCheckOutInfo(String address, Enum<RoomType> roomType) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RoomPO> checkOutPOs = new ArrayList<>();
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("select * from checkout where hotelAddress = ? and roomType = ?");
+			pstmt.setString(1, address);
+			pstmt.setInt(2, convertFromRoomTypeToInt(roomType));
+			rs = pstmt.executeQuery();
+			
+			//遍历结果，构造briefHotelInfoPO，并添加到列表中
+			while(rs.next()) {
+				CheckOutPO checkOutPO = new CheckOutPO(convertFromIntToRoomType(rs.getInt("roomType")), rs.getInt("roomNum"), address, rs.getTimestamp("actDepartTime"));
+				checkOutPOs.add(checkOutPO);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(rs, conn, pstmt);
+		}
+        return checkOutPOs;
     }
 
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     public void updateRoom(RoomPO po, Date day) throws RemoteException {
-        // TODO Auto-generated method stub
-        
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("update room" + (day.getMonth() + 1) + (day.getDate())+ " set roomNum = ?, roomPrice = ? where hotelAddress = ? and roomType = ?");
+			pstmt.setInt(1, po.getRoomNum());
+			pstmt.setInt(2, po.getRoomPrice());
+			pstmt.setString(3, po.getAddress());
+			pstmt.setInt(4, convertFromRoomTypeToInt(po.getRoomType()));
+			pstmt.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(null, conn, pstmt);
+		}
     }
 
     @Override
     public void insertRoom(RoomPO po) throws RemoteException {
-        // TODO Auto-generated method stub
-        
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			for(int i = 0; i < 7; i++){
+				Calendar calendar = Calendar.getInstance();
+				calendar.add(Calendar.DATE, i);
+				String day = "" + (calendar.get(Calendar.MONTH) + 1) + String.format("%02d", calendar.get(Calendar.DATE));
+				pstmt = conn.prepareStatement("insert into room" + day + "(hotelAddress, roomType, roomNum, roomPrice) values(?,?,?,?)");
+				pstmt.setString(1, po.getAddress());
+				pstmt.setInt(2, convertFromRoomTypeToInt(po.getRoomType()));
+				pstmt.setInt(3, po.getRoomNum());
+				pstmt.setInt(4, po.getRoomPrice());
+				pstmt.executeUpdate();
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(null, conn, pstmt);
+		}
     }
 
     @Override
     public void insertCheckIn(CheckInPO po) throws RemoteException {
-        // TODO Auto-generated method stub
-        
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("insert into checkin(hotelAddress, roomType, roomNum, checkInTime, expDepartTime) values(?,?,?,?,?)");
+			pstmt.setString(1, po.getAddress());
+			pstmt.setInt(2, convertFromRoomTypeToInt(po.getRoomType()));
+			pstmt.setInt(3, po.getRoomNum());
+			pstmt.setTimestamp(4, new java.sql.Timestamp(po.getCheckInTime().getTime()));
+			pstmt.setTimestamp(5, new java.sql.Timestamp(po.getExpDepartTime().getTime()));
+			pstmt.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(null, conn, pstmt);
+		}
     }
 
     @Override
     public void insertCheckOut(CheckOutPO po) throws RemoteException {
-        // TODO Auto-generated method stub
-        
+    	Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			//初始化数据库连接
+			conn = JDBC_Connection.getConnection();
+			//根据酒店地址获得数据库数据
+			pstmt = conn.prepareStatement("insert into checkout(hotelAddress, roomType, roomNum, actDepartTime) values(?,?,?,?)");
+			pstmt.setString(1, po.getAddress());
+			pstmt.setInt(2, convertFromRoomTypeToInt(po.getRoomType()));
+			pstmt.setInt(3, po.getRoomNum());
+			pstmt.setTimestamp(4, new java.sql.Timestamp(po.getActDepartTime().getTime()));
+			pstmt.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//释放数据库资源
+			JDBC_Connection.free(null, conn, pstmt);
+		}
     }
 
 }
---
+++ Codes/HotelReserveSystem_Server/src/test/java/roomdata/RoomDAOImplTest.java
@@ -0,0 +1,222 @@
+package roomdata;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import data.roomdata.RoomDAOImpl;
+import dataservice.roomDAO.RoomDAO;
+import po.CheckInPO;
+import po.CheckOutPO;
+import po.RoomPO;
+import po.RoomType;
+
+public class RoomDAOImplTest {
+
+	private RoomDAO roomDAO;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.roomDAO = new RoomDAOImpl();
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetSpareRoomInfoList() {
+		Date day = new Date(116, 10, 28);
+		try {
+			ArrayList<RoomPO> roomPOs = roomDAO.getSpareRoomInfoList("江苏省南京市栖霞区仙林大道163号", day);
+			assertEquals(4, roomPOs.size());
+			assertEquals(15, roomPOs.get(0).getRoomNum());
+			assertEquals(5, roomPOs.get(3).getRoomNum());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetSpareRoomInfo() {
+		Date day = new Date(116, 10, 28);
+		try {
+			RoomPO roomPO = roomDAO.getSpareRoomInfo("江苏省南京市栖霞区仙林大道163号", RoomType.KING_SIZE_ROOM, day);
+			assertEquals(5, roomPO.getRoomNum());
+			assertEquals(400, roomPO.getRoomPrice());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetCheckInInfoList() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		try {
+			ArrayList<RoomPO> roomPOs = roomDAO.getCheckInInfoList("江苏省南京市栖霞区仙林大道163号");
+			assertEquals(3, roomPOs.size());
+			Date checkInTime = new Date(116, 9, 05, 12, 00, 00);
+			Date expDepartTime = new Date(116, 9, 06, 12, 00, 00);
+			assertEquals(sdf.format(checkInTime), sdf.format(((CheckInPO)roomPOs.get(1)).getCheckInTime()));
+			assertEquals(sdf.format(expDepartTime), sdf.format(((CheckInPO)roomPOs.get(1)).getExpDepartTime()));
+			assertEquals(3, roomPOs.get(0).getRoomNum());
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetCheckInInfo1() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		try {
+			ArrayList<RoomPO> roomPOs = roomDAO.getCheckInInfo("江苏省南京市栖霞区仙林大道163号", RoomType.STANDARD_ROOM);
+			assertEquals(1, roomPOs.size());
+			Date checkInTime = new Date(116, 9, 05, 12, 00, 00);
+			Date expDepartTime = new Date(116, 9, 06, 12, 00, 00);
+			assertEquals(sdf.format(checkInTime), sdf.format(((CheckInPO)roomPOs.get(0)).getCheckInTime()));
+			assertEquals(sdf.format(expDepartTime), sdf.format(((CheckInPO)roomPOs.get(0)).getExpDepartTime()));
+			assertEquals(4, roomPOs.get(0).getRoomNum());
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetCheckInInfo2() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		Date startTime = new Date(116, 9, 5, 10, 00, 00);
+		Date endTime = new Date(116, 9, 5, 13, 00, 00);
+		try {
+			ArrayList<RoomPO> roomPOs = roomDAO.getCheckInInfo("江苏省南京市栖霞区仙林大道163号", startTime, endTime);
+			assertEquals(1, roomPOs.size());
+			Date checkInTime = new Date(116, 9, 05, 12, 00, 00);
+			Date expDepartTime = new Date(116, 9, 06, 12, 00, 00);
+			assertEquals(sdf.format(checkInTime), sdf.format(((CheckInPO)roomPOs.get(0)).getCheckInTime()));
+			assertEquals(sdf.format(expDepartTime), sdf.format(((CheckInPO)roomPOs.get(0)).getExpDepartTime()));
+			assertEquals(4, roomPOs.get(0).getRoomNum());
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetCheckOutInfoList() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		try {
+			ArrayList<RoomPO> roomPOs = roomDAO.getCheckOutInfoList("江苏省南京市栖霞区仙林大道163号");
+			assertEquals(3, roomPOs.size());
+			Date actDepartTime = new Date(116, 9, 07, 12, 00, 00);
+			assertEquals(sdf.format(actDepartTime), sdf.format(((CheckOutPO)roomPOs.get(0)).getActDepartTime()));
+			assertEquals(4, roomPOs.get(0).getRoomNum());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetCheckOutInfo1() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		try {
+			ArrayList<RoomPO> roomPOs = roomDAO.getCheckOutInfo("江苏省南京市栖霞区仙林大道163号", RoomType.STANDARD_ROOM);
+			assertEquals(1, roomPOs.size());
+			Date actDepartTime = new Date(116, 9, 07, 12, 00, 00);
+			assertEquals(sdf.format(actDepartTime), sdf.format(((CheckOutPO)roomPOs.get(0)).getActDepartTime()));
+			assertEquals(4, roomPOs.get(0).getRoomNum());
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetCheckOutInfo2() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		Date startTime = new Date(116, 10, 12, 10, 00, 00);
+		Date endTime = new Date(116, 10, 12, 13, 00, 00);
+		try {
+			ArrayList<RoomPO> roomPOs = roomDAO.getCheckOutInfo("江苏省南京市栖霞区仙林大道163号", startTime, endTime);
+			assertEquals(1, roomPOs.size());
+			Date actDepartTime = new Date(116, 10, 12, 12, 00, 00);
+			assertEquals(sdf.format(actDepartTime), sdf.format(((CheckOutPO)roomPOs.get(0)).getActDepartTime()));
+			assertEquals(3, roomPOs.get(0).getRoomNum());
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+//	@SuppressWarnings("deprecation")
+//	@Test
+//	public void testUpdateRoom() {
+//		RoomPO roomPO = new RoomPO(RoomType.SINGLE_ROOM, 50, 100, "江苏省南京市栖霞区仙林大道163号");
+//		Date day = new Date(116, 10, 29);
+//		try {
+//			roomDAO.updateRoom(roomPO, day);
+//		} catch (RemoteException e) {
+//			e.printStackTrace();
+//			fail("Not yet implemented");
+//		}
+//	}
+	
+//	@Test
+//	public void testInsertRoom() {
+//		RoomPO roomPO = new RoomPO(RoomType.SINGLE_ROOM, 50, 100, "江苏省南京市栖霞区仙林大道164号");
+//		try {
+//			roomDAO.insertRoom(roomPO);
+//		} catch (RemoteException e) {
+//			e.printStackTrace();
+//			fail("Not yet implemented");
+//		}
+//	}
+	
+//	@SuppressWarnings("deprecation")
+//	@Test
+//	public void testInsertCheckIn() {
+//		Date checkInTime = new Date(116, 10, 15, 13, 00, 00);
+//		Date expDepartTime = new Date(116, 10, 16, 13, 00, 00);
+//		CheckInPO checkInPO = new CheckInPO(RoomType.KING_SIZE_ROOM, 1, "江苏省南京市栖霞区仙林大道164号", checkInTime, expDepartTime);
+//		try {
+//			roomDAO.insertCheckIn(checkInPO);
+//		} catch (RemoteException e) {
+//			e.printStackTrace();
+//			fail("Not yet implemented");
+//		}
+//	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testInsertCheckOut() {
+		Date actDepartTime = new Date(116, 10, 16, 11, 00, 00);
+		CheckOutPO checkOutPO = new CheckOutPO(RoomType.KING_SIZE_ROOM, 1, "江苏省南京市栖霞区仙林大道164号", actDepartTime);
+		try {
+			roomDAO.insertCheckOut(checkOutPO);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+
+}
