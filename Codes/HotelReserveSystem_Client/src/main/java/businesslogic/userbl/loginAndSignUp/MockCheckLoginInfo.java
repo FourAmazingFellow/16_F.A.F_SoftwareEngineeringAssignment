b54836bd4abac17ba86c5b57b0ebdcc2be208791
sparkler <1040494975@qq.com>
2016-12-04 20:27:58 +0800

    modify

--- Codes/HotelReserveSystem_Client/src/main/java/businesslogic/userbl/loginAndSignUp/MockCheckLoginInfo.java
+++ Codes/HotelReserveSystem_Client/src/main/java/businesslogic/userbl/loginAndSignUp/MockCheckLoginInfo.java
@@ -12,17 +12,17 @@ public class MockCheckLoginInfo extends CheckLoginInfo{
     private UserPO userPO;
     private String password;
     
-    public boolean checkUser(String userID,String password) {
-        this.userID = userID;
-        try {
-            userPO = userDAO.getUserInfo(this.userID);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        this.password = userPO.getPassword();
-        if (this.password == password) {
-            return false;
-        } else
-            return true;
-    }
+//    public boolean checkUser(String userID,String password) {
+//        this.userID = userID;
+//        try {
+//            userPO = userDAO.getUserInfo(this.userID);
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
+//        this.password = userPO.getPassword();
+//        if (this.password == password) {
+//            return false;
+//        } else
+//            return true;
+//    }
 }
