ba7ecf8a215c4a7be6d42d707d08a1094147523d
Accident <842631684@qq.com>
2016-12-10 22:53:26 +0800

    上传ClientRootPanel的FXML供参考

---
+++ Codes/HotelReserveSystem_Client/src/main/java/presentation/mainui/ClientRootBoardController.java
@@ -0,0 +1,27 @@
+package presentation.mainui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+
+public class ClientRootBoardController {
+
+    @FXML
+    private MenuButton userActionButton;
+
+    @FXML
+    private Button modifyPersonalInfoSection;
+
+    @FXML
+    private Button myOrderSection;
+
+    @FXML
+    private Button myReservedHotelSection;
+
+    @FXML
+    private Button searchHotelSection;
+
+    @FXML
+    private Button homeButton;
+
+}
--- Codes/HotelReserveSystem_Client/src/main/java/presentation/orderui/DetailedOrderPanelController.java
+++ Codes/HotelReserveSystem_Client/src/main/java/presentation/orderui/DetailedOrderPanelController.java
@@ -12,58 +12,40 @@ import presentation.MainApp;
 import vo.OrderVO;
 
 public class DetailedOrderPanelController {
-
 	@FXML
 	private Label orderStateLabel;
-
 	@FXML
 	private Label isOnSaleLabel;
-
 	@FXML
 	private Button commentButton;
-
 	@FXML
 	private Label isCommentedLabel;
-
 	@FXML
 	private Label beginTimeLabel;
-
 	@FXML
 	private Label orderProducedTimeLabel;
-
 	@FXML
 	private Label orderIDLabel;
-
 	@FXML
 	private Label isChildrenLabel;
-
 	@FXML
 	private Label numOfPersonLabel;
-
 	@FXML
 	private Label hotelNameLabel;
-
 	@FXML
 	private Label hotelAddressLabel;
-
 	@FXML
 	private Label numLabel;
-
 	@FXML
 	private Label latestOrderDoneTimeLabel;
-
 	@FXML
 	private Label finishDateLabel;
-
 	@FXML
 	private Button withdrawOrderButton;
-
 	@FXML
 	private Button returnButton;
-
 	@FXML
 	private Label totalPrcieLabel;
-
 	@FXML
 	private Label roomTypeLabel;
 
