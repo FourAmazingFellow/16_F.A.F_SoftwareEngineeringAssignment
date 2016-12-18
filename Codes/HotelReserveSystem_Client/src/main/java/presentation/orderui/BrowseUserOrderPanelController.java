479eb5a5edd051c06e36338ee3efb183a329e6d0
Accident <842631684@qq.com>
2016-12-18 16:16:18 +0800

    Revert "HotelSearch"
    This reverts commit 451d6b669bd5d94ca5efeac5d753ec67fc2bb637.

--- Codes/HotelReserveSystem_Client/src/main/java/presentation/orderui/BrowseUserOrderPanelController.java
+++ Codes/HotelReserveSystem_Client/src/main/java/presentation/orderui/BrowseUserOrderPanelController.java
@@ -78,6 +78,7 @@ public class BrowseUserOrderPanelController {
 		orderTypeChoiceBox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				System.out.println(OrderType.values()[(int) newValue]);
 				getBriefOrderList(ClientMainApp.userID, OrderType.values()[(int) newValue]);
 			}
 		});
