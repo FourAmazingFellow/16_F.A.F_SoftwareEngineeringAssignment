d2bb8da00174e074f107ab649990a2d5a99e89fa
sparkler <1040494975@qq.com>
2016-12-18 17:16:44 +0800

    reservedHotel

--- Codes/HotelReserveSystem_Client/src/main/java/presentation/hotelui/reservedhotel/ReservedHotelCintroller.java
+++ Codes/HotelReserveSystem_Client/src/main/java/presentation/hotelui/reservedhotel/ReservedHotelCintroller.java
@@ -4,19 +4,32 @@ import businesslogicservice.hotelblservice.CheckOrderedHotelService;
 import businesslogicservice.hotelblservice.ManageHotelInfoService;
 import factory.HotelUIFactoryService;
 import factory.HotelUIFactoryServiceImpl;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import po.OrderType;
+import presentation.ClientMainApp;
 import presentation.HotelMainApp;
+import presentation.orderui.FxBriefOrder;
 
 public class ReservedHotelCintroller {
 	private HotelUIFactoryService hotelFactory;
 	private CheckOrderedHotelService checkOrderedHotel;
 	private HotelMainApp mainApp;
+	private ObservableList<FxBriefOrder> orderList = FXCollections.observableArrayList();
 
+	@FXML
+	private TabPane orderListTab;
+	@FXML
+	private Tab allTab;
 	@FXML
 	private Tab abnormalTab;
 
@@ -27,38 +40,94 @@ public class ReservedHotelCintroller {
 	private Tab normalTab;
 
 	@FXML
-	private TableView<?> orderDetailTableAbnormal;
-
-	@FXML
-	private TableView<?> orderDetailTableNormal;
-
-	@FXML
-	private TabPane orderListTab;
-
-	@FXML
-	private TableView<?> orderDetailTableAll;
+	private Button hotelDetailButton;
 
 	@FXML
 	private Button orderDoneButton;
 
 	@FXML
-	private Tab allTab;
+	private TableView<?> orderDetailTableAll;
+
+	@FXML
+	private TableView<?> orderDetailTableNormal;
+
+	@FXML
+	private TableView<?> orderDetailTableAbmormal;
 
 	@FXML
 	private TableView<?> orderDetailTableUndo;
 
 	@FXML
-	private Button hotelDetailButton;
+	private TableColumn<?, ?> tagColumn2;
 
 	@FXML
-	void hotelDetailButtonAction(ActionEvent event) {
-
-	}
+	private TableColumn<?, ?> tagColumn3;
 
 	@FXML
-	void orderDoneButtonAction(ActionEvent event) {
+	private TableColumn<?, ?> tagColumn1;
 
-	}
+	@FXML
+	private TableColumn<?, ?> hotelAddressColumn;
+
+	@FXML
+	private TableColumn<?, ?> tagColumn;
+
+	@FXML
+	private TableColumn<?, ?> tradeAreaColumn3;
+
+	@FXML
+	private TableColumn<?, ?> tradeAreaColumn2;
+
+	@FXML
+	private TableColumn<?, ?> tradeAreaColumn;
+
+	@FXML
+	private TableColumn<?, ?> tradeAreaColumn1;
+
+	@FXML
+	private TableColumn<?, ?> hotelStarColumn;
+
+	@FXML
+	private TableColumn<?, ?> hotelAddressColumn2;
+
+	@FXML
+	private TableColumn<?, ?> hotelAddressColumn3;
+
+	@FXML
+	private TableColumn<?, ?> hotelNameColumn1;
+
+	@FXML
+	private TableColumn<?, ?> hotelNameColumn2;
+
+	@FXML
+	private TableColumn<?, ?> hotelNameColumn3;
+
+	@FXML
+	private TableColumn<?, ?> hotelAddressColumn1;
+
+	@FXML
+	private TableColumn<?, ?> hotelStarColumn1;
+
+	@FXML
+	private TableColumn<?, ?> hotelMarkColumn1;
+
+	@FXML
+	private TableColumn<?, ?> hotelStarColumn3;
+
+	@FXML
+	private TableColumn<?, ?> hotelStarColumn2;
+
+	@FXML
+	private TableColumn<?, ?> hotelMarkColumn2;
+
+	@FXML
+	private TableColumn<?, ?> hotelMarkColumn3;
+
+	@FXML
+	private TableColumn<?, ?> hotelNameColumn;
+
+	@FXML
+	private TableColumn<?, ?> hotelMarkColumn;
 
 	@FXML
 	void initialize() {
@@ -68,5 +137,15 @@ public class ReservedHotelCintroller {
 
 	public void setMainApp(HotelMainApp mainApp) {
 		this.mainApp = mainApp;
+	}
+	
+	@FXML
+	void hotelDetailButtonAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void orderDoneButtonAction(ActionEvent event) {
+
 	}
 }
