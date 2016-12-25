bd01c57ace9dff5bf52cf31bdd9f4f474cbdea29
Accident <842631684@qq.com>
2016-12-25 22:18:24 +0800

    服务器端PO注释添加

--- Codes/HotelReserveSystem_Server/src/main/java/po/OrderState.java
+++ Codes/HotelReserveSystem_Server/src/main/java/po/OrderState.java
@@ -2,6 +2,7 @@ package po;
 
 /**
  * 订单状态枚举类型
+ * 异常订单、未执行订单、已执行订单、已撤销订单
  * @author Accident
  * @version 
  * @see
--- Codes/HotelReserveSystem_Server/src/main/java/po/OrderType.java
+++ Codes/HotelReserveSystem_Server/src/main/java/po/OrderType.java
@@ -2,6 +2,7 @@ package po;
 
 /**
  * 订单类型枚举类型
+ * 全部订单、异常订单、未执行订单、已执行订单、已撤销订单
  * @author Accident
  * @version 1.0
  * @see
